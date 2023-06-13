1ad8e7b75146b9a0268fedd382cad64bb33e61c1
Truong Hiep Hung <hungjt46@gmail.com>
2023-06-13 16:35:46 +0700

    feat: profile page

---
+++ FUFlowerSystem/FlowerBouquetManagement/Pages/Account/Profile.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FlowerBouquetManagement.Pages.Account
+{
+    public class ProfileModel : PageModel
+    {
+        private readonly ICustomerRepository _repo;
+
+        public ProfileModel(ICustomerRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [BindProperty]
+        public DataLayer.Models.Customer Customer { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            string role = HttpContext.Session.GetString("ROLE");
+            int id = 1;
+            if (id == null || _repo.GetAll == null)
+            {
+                return NotFound();
+            }
+
+            var customer = _repo.Get((int)id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            Customer = customer;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _repo.UpdateCustomer(Customer);
+
+            return RedirectToPage("../Index");
+        }
+    }
+}
